2de7cf2d6961aaa62887d8cac7ea319848f000c8
kingwell47 <joeldoctor47@gmail.com>
2021-12-01 19:45:43 +0800

    removed tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import { Routes, Route } from "react-router-dom";
 import DUMMY_DATA from "./dummy_data.json";
 import NewProject from "./components/NewProject";
 import Project from "./components/Project";
+import Login from "./components/Login";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" exact element={<Projects data={DUMMY_DATA} />} />
+        <Route path="/login" exact element={<Login />} />
         <Route path="/new-project" exact element={<NewProject />} />
         <Route path="/projects/:id" exact element={<Project />} />
       </Routes>
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 const Login = () => {
-  return (
-    <div>
-      <h2>LOGIN HERE</h2>
-    </div>
-  );
+  return <div>LOGIN</div>;
 };
 
 export default Login;
