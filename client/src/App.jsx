298caf9d875e89fd72ee57745ebeec2a62dda01a
kingwell47 <joeldoctor47@gmail.com>
2021-12-02 19:04:47 +0800

    implemented some material UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,32 @@
-import Navbar from "./components/Navbar";
-import Projects from "./components/Projects";
 import { Routes, Route } from "react-router-dom";
 
-import DUMMY_DATA from "./dummy_data.json";
+import AppNavbar from "./components/AppNavbar";
+import Projects from "./components/Projects";
 import NewProject from "./components/NewProject";
 import Project from "./components/Project";
 import Login from "./components/Login";
+import SideBar from "./components/SideBar";
+
+import Box from "@mui/material/Box";
+
+import DUMMY_DATA from "./dummy_data.json";
 
 function App() {
   return (
-    <div className="container mx-auto">
-      <Navbar />
-      <Routes>
-        <Route path="/" exact element={<Projects data={DUMMY_DATA} />} />
-        <Route path="/login" exact element={<Login />} />
-        <Route path="/new-project" exact element={<NewProject />} />
-        <Route path="/projects/:id" exact element={<Project />} />
-      </Routes>
-    </div>
+    <>
+      <AppNavbar />
+      <Box sx={{ display: "flex", paddingTop: 10, gap: 5 }}>
+        <SideBar />
+        <Box sx={{ paddingLeft: 30 }}>
+          <Routes>
+            <Route path="/" exact element={<Projects data={DUMMY_DATA} />} />
+            <Route path="/login" exact element={<Login />} />
+            <Route path="/new-project" exact element={<NewProject />} />
+            <Route path="/projects/:id" exact element={<Project />} />
+          </Routes>
+        </Box>
+      </Box>
+    </>
   );
 }
 
