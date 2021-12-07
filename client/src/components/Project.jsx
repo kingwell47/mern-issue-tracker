8d12334c39c3c75c37b19865c21b279de2b68fd3
kingwell47 <joeldoctor47@gmail.com>
2021-12-07 19:10:09 +0800

    added data grid to project

--- client/src/components/Project.jsx
+++ client/src/components/Project.jsx
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { DataGrid } from "@mui/x-data-grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+
 import DUMMY_DATA from "../dummy_data.json";
 
 const Project = () => {
@@ -15,27 +20,66 @@ const Project = () => {
     setProjectData(project[0]);
   }, []);
 
+  const columns = [
+    { field: "id", headerName: "ID", width: 90 },
+    {
+      field: "firstName",
+      headerName: "First name",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "lastName",
+      headerName: "Last name",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "number",
+      width: 110,
+      editable: true,
+    },
+    {
+      field: "fullName",
+      headerName: "Full name",
+      description: "This column has a value getter and is not sortable.",
+      sortable: false,
+      width: 160,
+      valueGetter: (params) =>
+        `${params.getValue(params.id, "firstName") || ""} ${
+          params.getValue(params.id, "lastName") || ""
+        }`,
+    },
+  ];
+
+  const rows = [
+    { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+    { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+    { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+    { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+    { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+    { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+    { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  ];
+
   return (
-    <div className="pt-20 px-3">
-      <h2 className="text-center font-bold text-xl">{projectData.project}</h2>
-      <p>{projectData.description}</p>
-      <ul>
-        {projectData.issues.map((item, index) => (
-          <li key={index}>
-            <div className="bg-blue-200 mt-2 rounded-md p-2">
-              <h3 className="text-center font-bold">{item.issue_title}</h3>
-              <p>{item.issue_text}</p>
-              <p>Created by: {item.created_by}</p>
-              <p>Assigned to: {item.assigned_to}</p>
-              <p>Open: {item.open.toString()}</p>
-              <p>Status text: {item.status_text}</p>
-              <p>Created on: {item.created_on}</p>
-              <p>Last updated: {item.updated_on}</p>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Box sx={{ height: "50rem", width: "100%" }}>
+      <Typography>{projectData.project}</Typography>
+      <Typography>{projectData.description}</Typography>
+      <Divider sx={{ mb: 4, mt: 3 }} />
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={15}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+        disableSelectionOnClick
+      />
+    </Box>
   );
 };
 
