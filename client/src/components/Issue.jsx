3f8cace86c4c05181b5bcd8401cfe6b2e0082a7a
kingwell47 <joeldoctor47@gmail.com>
2021-12-11 13:25:33 +0800

    implemented info table

--- client/src/components/Issue.jsx
+++ client/src/components/Issue.jsx
@@ -3,6 +3,15 @@ import { useParams } from "react-router-dom";
 
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Avatar from "@mui/material/Avatar";
+
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
 
 import DUMMY_DATA from "../dummy_data.json";
 
@@ -14,22 +23,45 @@ const Issue = () => {
     (item) => item.id === parseInt(issueId)
   );
   const { id } = issue[0];
+
+  function createData(name, info) {
+    return { name, info };
+  }
+
+  const rows = [
+    createData("Created by", "Username"),
+    createData("Assigned to", "User assigned"),
+    createData("Status", "Open"),
+    createData("Priority", "Low"),
+    createData("Status info", "Under investigation"),
+  ];
+
   return (
     <Paper sx={{ p: 2, display: "flex" }}>
-      <Box border="1px solid red" p={2}>
-        <div>Created By</div>
-        <div>Assigned To</div>
-        <div>Status</div>
-        <div>Priority</div>
-        <div>Status Text</div>
-      </Box>
-      <Box border="1px solid red" p={2}>
+      <TableContainer sx={{ width: 300 }}>
+        <Table sx={{ width: 300 }} size="small" aria-label="a dense table">
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="left">{row.info}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Box p={2}>
         <div>IssueID {id}</div>
         <div>Issue Text</div>
         <div>Created On</div>
         <div>Last Updated</div>
       </Box>
-      <Box border="1px solid red" p={2}>
+      <Box p={2}>
         <div>Comments</div>
       </Box>
     </Paper>
