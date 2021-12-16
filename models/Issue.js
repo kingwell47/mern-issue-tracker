b34ee34646251126737507f2abfa25cdc3cc5979
kingwell47 <joeldoctor47@gmail.com>
2021-12-16 21:26:41 +0800

    created new issue scheme with comments

---
+++ models/Issue.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const issueSchema = new mongoose.Schema(
+  {
+    issue_title: { type: String, required: true },
+    issue_text: { type: String, required: true },
+    created_by: { type: String, required: true },
+    assigned_to: String,
+    open: { type: Boolean, default: true },
+    priority: String,
+    comments: [String],
+    status_text: String,
+  },
+  {
+    timestamps: {
+      currentTime: () => new Date().toJSON(),
+      createdAt: "created_on",
+      updatedAt: "updated_on",
+    },
+  }
+);
